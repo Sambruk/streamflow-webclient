0f91f6d8241de64b0c27e20a7a77a864fc8a1f56
Katarina Hallberg <katarina.hallberg@jayway.com>
2015-04-01 08:24:46 +0200

    Removed unused function submitContact
    Could not find any usage of this function anymore.

--- app/routes/cases/contact/contacteditctrl.js
+++ app/routes/cases/contact/contacteditctrl.js
@@ -36,29 +36,6 @@ angular.module('sf')
       }
     });
 
-    $scope.submitContact = function($event){
-      $event.preventDefault();
-
-      // Unfortunate API weirdness that demands manual object conversion.
-      $scope.contact[0].address = $scope.contact[0].addresses[0].address;
-      $scope.contact[0].city = $scope.contact[0].addresses[0].city;
-      $scope.contact[0].country = $scope.contact[0].addresses[0].country;
-      $scope.contact[0].region = $scope.contact[0].addresses[0].region;
-      $scope.contact[0].zipCode = $scope.contact[0].addresses[0].zipCode;
-      $scope.contact[0].email = $scope.contact[0].emailAddresses[0].emailAddress;
-      $scope.contact[0].phone = $scope.contact[0].phoneNumbers[0].phoneNumber;
-      $scope.contact[0].contactpreference = $scope.contact[0].contactPreference;
-
-      $scope.contactId = caseService.updateContact($routeParams.caseId, $routeParams.contactIndex, $scope.contact[0])
-      .then(function(){
-        var href = navigationService.caseHref($routeParams.caseId);
-        $scope.contact.invalidate();
-        $scope.contact.resolve();
-
-        window.location.assign(href);
-      });
-    };
-
     $scope.updateField = function ($event, $success, $error) {
       $event.preventDefault();
       var contact = {};

1462c7172bb5c7e105fcdbf86e3725ee1acc7ac3
Erik Rydgren <erik.rydgren@jayway.com>
2015-08-28 12:48:43 +0200

    1) Added About Streamflow page. 2) Changed bower dependencies to fix broken build.

---
+++ app/routes/about/aboutctrl.js
@@ -0,0 +1,31 @@
+/*
+ *
+ * Copyright 2009-2015 Jayway Products AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+angular.module('sf').controller('AboutCtrl', function ($scope, profileService, $rootScope, httpService) {
+  $rootScope.$broadcast('breadcrumb-updated', [
+  {
+    title: 'Om Streamflow'
+  }]);
+
+  httpService.getRequest("static/version.html", false)
+    .then(function(result){
+      var versionMatch = /(Version: )[a-z,A-Z,\d,.,-]+/.exec(result.data);
+      if(versionMatch && versionMatch.length > 0 && versionMatch[0].length > 9) {
+        $scope.serverVersion = versionMatch[0].substr(9);
+      }
+  });
+});
--- app/routes/appRoutes.js
+++ app/routes/appRoutes.js
@@ -40,6 +40,10 @@ angular.module('sf')
       templateUrl: 'routes/profile/profile-edit.html',
       controller: 'ProfileEditCtrl'
     })
+    .when('/about', {
+      templateUrl: 'routes/about/about.html',
+      controller: 'AboutCtrl'
+    })
     .when('/projects/:projectId/:projectType', {
       templateUrl: 'routes/projects/caselist.html',
       controller: 'CaseListCtrl'

a471d836f745b3106205a96c2dd21f5dbc099b1c
3emluk <3emluk@gmail.com>
2016-09-19 18:58:39 +0300

    SF-25: Little optimization and added some regex work around.

--- app/components/sidebar/sidebar-case-type-directive.js
+++ app/components/sidebar/sidebar-case-type-directive.js
@@ -36,53 +36,27 @@ angular.module('sf').directive('sidebarCaseType', function (sidebarService) {
                     return map[m];
                 });
             };
-            scope.escapeReverseHTMLChars = function (value) {
-                value = value.replace(new RegExp('&lt;','g'), "<")
-                    .replace(new RegExp('&gt;','g'), ">")
-                    .replace(new RegExp('&quot;','g'), '"')
-                    .replace(new RegExp('&#039;','g'), "'");
-                return value;
-            };
-
 
             scope.swapHTMLChars = function (value) {
-          /*      var map = {
-                    '<': '&1lt;',
-                    '>': '&1gt;',
-                    '"': '&1quot;',
-                    "'": '&1#039;',
-                    '&lt;': '<',
-                    '&gt;': '>',
-                    '&quot;': '"',
-                    '&#039;': "'",
-                    '&1lt;': '&lt;',
-                    '&1qt;': '&gt;',
-                    '&qquot;': '&quot;',
-                    '&q#039;': '&#039;'
-                };
-                return value.replace(/[&<>"']/g, function (m) {
-                    return map[m];
-                });*/
 
                 value = value
-                    .replace(new RegExp('<','g'), '&1lt;')
-                    .replace(new RegExp('>','g'), '&1gt;')
-                    .replace(new RegExp('";','g'), '&1quot')
-                    .replace(new RegExp("'",'g'), '&1#039;')
+                    .replace(new RegExp('<', 'g'), '&1lt;')
+                    .replace(new RegExp('>', 'g'), '&1gt;')
+                    .replace(new RegExp('";', 'g'), '&1quot')
+                    .replace(new RegExp("'", 'g'), '&1#039;')
 
-                    .replace(new RegExp('&lt;','g'), "<")
-                    .replace(new RegExp('&gt;','g'), ">")
-                    .replace(new RegExp('&quot;','g'), '"')
-                    .replace(new RegExp('&#039;','g'), "'")
+                    .replace(new RegExp('&lt;', 'g'), "<")
+                    .replace(new RegExp('&gt;', 'g'), ">")
+                    .replace(new RegExp('&quot;', 'g'), '"')
+                    .replace(new RegExp('&#039;', 'g'), "'")
 
 
-                    .replace(new RegExp('&1lt;','g'), '&lt;')
-                    .replace(new RegExp('&1gt;','g'), '&gt;')
-                    .replace(new RegExp('&1quot;','g'), '&quot;')
-                    .replace(new RegExp('&1#039;','g'), '&#039;');
+                    .replace(new RegExp('&1lt;', 'g'), '&lt;')
+                    .replace(new RegExp('&1gt;', 'g'), '&gt;')
+                    .replace(new RegExp('&1quot;', 'g'), '&quot;')
+                    .replace(new RegExp('&1#039;', 'g'), '&#039;');
                 return value;
             };
-
 
             //Not the best implementation too many regex and as result too heavy
             scope.highlight = function (text, search) {
@@ -95,13 +69,23 @@ angular.module('sf').directive('sidebarCaseType', function (sidebarService) {
                 var openHighlightTag = '<span class="ui-select-highlight">';
                 var closeHighlightTag = '</span>';
 
+                search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+
                 if (htmlChars.some(function (v) {
                         return search.indexOf(v) >= 0;
                     })) {
                     var tmpSearchStr = scope.escapeHTMLChars(search);
-                    return (scope.escapeHTMLChars(text).replace(new RegExp((search), 'gi'), (search)).replace(new RegExp((tmpSearchStr), 'gi'), openHighlightTag  +'$&' + closeHighlightTag));
+                    return (scope.escapeHTMLChars(text).replace(new RegExp((search), 'gi'), (search)).replace(new RegExp((tmpSearchStr), 'gi'), openHighlightTag + '$&' + closeHighlightTag));
                 } else {
-                    return  scope.swapHTMLChars((text).replace(new RegExp((search), 'gi'), scope.escapeHTMLChars(openHighlightTag) + '$&' + scope.escapeHTMLChars(closeHighlightTag )));
+                    //Bad but working way
+                    return scope.swapHTMLChars((text).replace(new RegExp((search), 'gi'), scope.escapeHTMLChars(openHighlightTag) + '$&' + scope.escapeHTMLChars(closeHighlightTag)));
+
+                    //RegexWay
+                    /*//(t)(?![^&;]*;)
+                     //Not matching chars before; trouble with q
+                     // (?!\s&)(t)(?![^\s;]?;)
+
+                     return (scope.escapeHTMLChars(text).replace(new RegExp('(?!&\S)(' + search +')(?![^\s;]?;)', 'gi'), (openHighlightTag) + '$&' + (closeHighlightTag) ));*/
                 }
             };
 

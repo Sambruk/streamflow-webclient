da66c3a0e84dad75daa0f655caceafb8adb3e3c0
3emluk <3emluk@gmail.com>
2017-04-18 17:58:19 +0300

    SF-44: Apply new UI to the web client: - Add empty note message

--- app/components/sidebar/sidebar-case-directive.js
+++ app/components/sidebar/sidebar-case-directive.js
@@ -9,6 +9,7 @@ angular.module('sf').directive('sidebarCase', function ($routeParams, httpServic
       scope.caze = scope.$parent.caze;
       scope.notes = scope.$parent.notes;
       scope.status  = $routeParams.status;
+      scope.emptyNoteMessage = 'Här visas automatiskt sammanfattningen, som normalt är starten på det första inlägget i ärendet. Du kan ändra sammanfattningen genom att klicka på pennan.';
 
       scope.showCaseInfo = false;
 

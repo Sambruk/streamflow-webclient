ea754b0d443c5c0be76a2e1054b050a2976d0390
3emluk <3emluk@gmail.com>
2017-04-19 16:43:01 +0300

    SF-44: Apply new UI to the web client: - Add autosave chekbox;

--- app/routes/cases/case-edit/caseeditctrl.js
+++ app/routes/cases/case-edit/caseeditctrl.js
@@ -23,6 +23,7 @@ angular.module('sf').controller('CaseEditCtrl', function ($scope, $rootScope, $r
     $scope.projectId = $routeParams.projectId;
     $scope.notesHistory = caseService.getAllNotes($routeParams.caseId);
     $scope.caze = caseService.getSelected($routeParams.caseId);
+    $scope.autoSave = false;
 
     $scope.$watch('sidebardata.caze', function (newVal) {
         if (!newVal) {

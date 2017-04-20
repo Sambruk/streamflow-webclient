e352f2a4c0d599236d9471a342490c6e7ab62b19
3emluk <3emluk@gmail.com>
2017-04-20 17:25:00 +0300

    SF-44: Apply new UI to the web client: - Reformat

--- app/components/caselogentry/caselogentrydirective.js
+++ app/components/caselogentry/caselogentrydirective.js
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-   *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,45 +17,45 @@
 'use strict';
 
 angular.module('sf')
-.directive('caselogentry', function($rootScope, $location, $routeParams, $q, $window, caseService, navigationService, checkPermissionService){
-  return {
-    restrict: 'E',
-    templateUrl: 'components/caselogentry/caselogentry.html',
-    scope: {
-      caseid: '=?'
-    },
-    link: function(scope){
-      scope.caseLogCommands = caseService.getCaseLogCommands($routeParams.caseId);
-      scope.caseLogCommands.promise.then(function(){
-        checkPermissionService.checkPermissions(scope, scope.caseLogCommands.commands, ['addmessage'], ['canAddCaseLogMessage']);
-      });
+    .directive('caselogentry', function ($rootScope, $location, $routeParams, $q, $window, caseService, navigationService, checkPermissionService) {
+        return {
+            restrict: 'E',
+            templateUrl: 'components/caselogentry/caselogentry.html',
+            scope: {
+                caseid: '=?'
+            },
+            link: function (scope) {
+                scope.caseLogCommands = caseService.getCaseLogCommands($routeParams.caseId);
+                scope.caseLogCommands.promise.then(function () {
+                    checkPermissionService.checkPermissions(scope, scope.caseLogCommands.commands, ['addmessage'], ['canAddCaseLogMessage']);
+                });
 
-      scope.caseLogEntryToCreate = '';
-      scope.caseId = $routeParams.caseId;
-      scope.caseLogs = null;
+                scope.caseLogEntryToCreate = '';
+                scope.caseId = $routeParams.caseId;
+                scope.caseLogs = null;
 
-      scope.$watch('caseLogs', function(newVal){
-        if(!newVal){
-          return;
-        }
-        scope.caseLogs = newVal;
-      });
+                scope.$watch('caseLogs', function (newVal) {
+                    if (!newVal) {
+                        return;
+                    }
+                    scope.caseLogs = newVal;
+                });
 
-      scope.submitCaseLogEntry = function($event){
-        $event.preventDefault();
-          if (scope.caseLogEntryToCreate) {
-            if(scope.$parent.status =='new' && scope.$parent.caseLogs.length == 0){
-                scope.$parent.notes[0].note = scope.caseLogEntryToCreate;
-              caseService.addNote(scope.caseId, scope.$parent.notes[0]);
-                $window.location.href = '#/cases/'+scope.$parent.caze[0].id+'/'+scope.$parent.caze[0].ownerId+'/notes';
+                scope.submitCaseLogEntry = function ($event) {
+                    $event.preventDefault();
+                    if (scope.caseLogEntryToCreate) {
+                        if (scope.$parent.status == 'new' && scope.$parent.caseLogs.length == 0) {
+                            scope.$parent.notes[0].note = scope.caseLogEntryToCreate;
+                            caseService.addNote(scope.caseId, scope.$parent.notes[0]);
+                            $window.location.href = '#/cases/' + scope.$parent.caze[0].id + '/' + scope.$parent.caze[0].ownerId + '/notes';
+                        }
+                        caseService.createCaseLogEntry(scope.caseId, scope.caseLogEntryToCreate)
+                            .then(function (response) {
+                                $rootScope.$broadcast('caselog-message-created');
+                                scope.caseLogEntryToCreate = '';
+                            });
+                    }
+                };
             }
-              caseService.createCaseLogEntry(scope.caseId, scope.caseLogEntryToCreate)
-                  .then(function (response) {
-                      $rootScope.$broadcast('caselog-message-created');
-                      scope.caseLogEntryToCreate = '';
-                  });
-          }
-      };
-    }
-  };
-});
+        };
+    });

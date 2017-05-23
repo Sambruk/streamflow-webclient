9c13db1129b6782270f7ab113a23cbf429817179
3emluk <3emluk@gmail.com>
2017-05-23 17:01:14 +0300

    SF-51: Status filter not applied: - Add status filter select (hardcoded values); - Add required logic to filter by status

--- app/components/search/search-directive.js
+++ app/components/search/search-directive.js
@@ -45,6 +45,7 @@ angular.module('sf').directive('search', function ($location, $timeout, searchSe
                 scope.group.order = 'asc';
                 scope.sort = {};
                 scope.sort.order = 'asc';
+                scope.status = '';
 
                 scope.groupingOptions = groupByService.getGroupingOptions();
 
@@ -100,6 +101,9 @@ angular.module('sf').directive('search', function ($location, $timeout, searchSe
                         } else if (result[0] === 'label') {
                             scope.filter.label = scope.filter.label || [];
                             scope.filter.label.push(result[1]);
+                        } else if (result[0] === 'status') {
+                            scope.filter.status = scope.filter.status || [];
+                            scope.filter.status.push(result[1]);
                         } else {
                             scope.filter[result[0]] = result[1];
                         }

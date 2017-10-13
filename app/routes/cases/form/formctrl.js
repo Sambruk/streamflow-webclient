64f5dca1bd31aab548f8126a2046cc6676384579
3emluk <3emluk@gmail.com>
2017-10-13 18:47:36 +0300

    SF-73: Change the logic of creating forms: - Add window reload on form submit.

--- app/routes/cases/form/formctrl.js
+++ app/routes/cases/form/formctrl.js
@@ -246,6 +246,7 @@ angular.module('sf')
                     $rootScope.$broadcast('form-saved', $scope.currentFormId);
                 }
             }
+            window.location.href = '#/cases/' + $scope.caseId + '/formhistory/' + $scope.currentFormId
             $scope.form = [];
             $scope.currentFormPage = null;
         };

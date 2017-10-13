dd2343dd5f11f0f338c894853db3aa3926904cd4
3emluk <3emluk@gmail.com>
2017-10-13 12:13:48 +0300

    SF-73: Change the logic of creating forms: - Obtain submitted form list from parent if not get from server(when in new window); - Remove unneeded event handler

--- app/components/sidebar/sidebar-forms-collapsed-directive.js
+++ app/components/sidebar/sidebar-forms-collapsed-directive.js
@@ -8,17 +8,13 @@ angular.module('sf').directive('sidebarFormsCollapsed', function ($routeParams, 
         },
         templateUrl: 'components/sidebar/sidebar-forms-collapsed.html',
         link: function (scope) {
-            scope.submittedFormList = caseService.getSubmittedFormList($routeParams.caseId);
+            scope.submittedFormList = scope.$parent.submittedFormList ? scope.$parent.submittedFormList : caseService.getSubmittedFormList($routeParams.caseId);
             scope.possibleForms = scope.$parent.possibleForms;
             scope.caze = scope.$parent.caze;
 
             scope.openFormInNewWindow = function (formId, newWindow) {
                 sidebarService.openForm(scope, formId, newWindow);
             };
-
-            scope.$on('form-saved', function(event, formId){
-                scope.submittedFormList =  caseService.getSubmittedFormList($routeParams.caseId);
-            });
         }
     };
 });

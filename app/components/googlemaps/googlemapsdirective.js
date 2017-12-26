10259384821ddd6837f191f9902fd842c4c3b8fd
3emluk <3emluk@gmail.com>
2017-12-26 18:36:52 +0200

    SF-87:  Problem saving point in map: - Now basic drawing options and zoom are applied correctly

--- app/components/googlemaps/googlemapsdirective.js
+++ app/components/googlemaps/googlemapsdirective.js
@@ -26,187 +26,7 @@ angular.module('sf').directive('googleMap', function () {
             locationSettings: '=',
             fieldSettings: '='
         },
-        compile: function($scope, scope,$parent) {
-
-
-            console.log('scope',$scope);
-            console.log('scope2',scope);
-            console.log('scope3', $parent);
-
-            function MapValue(mapFieldValue) {
-                this.path = [];
-                this.value = mapFieldValue;
-
-                return this;
-            };
-
-            MapValue.prototype.clearAddress = function () {
-                var self = this;
-                self.value.street = '';
-                self.value.zipcode = '';
-                self.value.city = '';
-                self.value.country = '';
-            };
-
-            MapValue.prototype.updateLocation = function (newLocation) {
-                var self = this;
-                self.path = [];
-                self.value.location = newLocation;
-                self.isPoint = false;
-                self.isPolyline = false;
-                self.isPolygon = false;
-
-                if (newLocation) {
-                    if (newLocation.indexOf('(') == -1) {
-                        self.isPoint = true;
-                        self.path.push(new LatLong(newLocation));
-
-                    } else {
-                        $.each(newLocation.split('),'), function (index, position) {
-                            self.path.push(new LatLong(position));
-                        });
-                        // If first point is the same as the last one its a polygon
-                        if (self.path[0].equals(self.path[self.path.length - 1])) {
-                            self.isPolygon = true;
-                        } else {
-                            self.isPolyline = true;
-                        }
-                    }
-                }
-            };
-
-
-            function LatLong(fieldValueString) {
-                var latLong = fieldValueString.split(',');
-                this.latitude = cleanUpPosition(latLong[0]);
-                this.longitude = cleanUpPosition(latLong[1]);
-            }
-
-            LatLong.prototype.equals = function (second) {
-                return this.latitude === second.latitude && this.longitude === second.longitude;
-            };
-
-
-            var initMap = function () {
-                $scope.mapValue = createMapValue($scope.ngModel);
-
-                // TODO: Read startPosition and zoomLevel from settings
-
-                var mapOptions = {
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    panControl: false,
-                    streetViewControl: false,
-                    overviewMapControl: false,
-                    scrollwheel: false
-                };
-
-                $scope.map = {
-                    // center: startPosition,
-                    zoom: 6,
-                    options: mapOptions,
-                    control: {},
-                    markersControl: {}
-                };
-
-                console.log($scope.map);
-                $scope.modelLoaded = true;
-
-
-                var availableDrawingModes = [];
-                var initialDrawingMode = null;
-
-                if ($scope.mapValue) {
-                    if ($scope.mapValue.isPoint) {
-                        initialDrawingMode = google.maps.drawing.OverlayType.MARKER;
-                    } else if ($scope.mapValue.isPolyline) {
-                        initialDrawingMode = google.maps.drawing.OverlayType.POLYLINE;
-                    } else if ($scope.mapValue.isPolygon) {
-                        initialDrawingMode = google.maps.drawing.OverlayType.POLYGON;
-                    }
-                }
-
-                // if ($scope.fieldSettings.point) {
-                //     availableDrawingModes.push(google.maps.drawing.OverlayType.MARKER);
-                //     if (!initialDrawingMode) {
-                //         initialDrawingMode = google.maps.drawing.OverlayType.MARKER;
-                //     }
-                // }
-                // if ($scope.fieldSettings.polyline) {
-                //     availableDrawingModes.push(google.maps.drawing.OverlayType.POLYLINE);
-                //     if (!initialDrawingMode) {
-                //         initialDrawingMode = google.maps.drawing.OverlayType.POLYLINE;
-                //     }
-                // }
-                // if ($scope.fieldSettings.polygon) {
-                //     availableDrawingModes.push(google.maps.drawing.OverlayType.POLYGON);
-                //     if (!initialDrawingMode) {
-                //         initialDrawingMode = google.maps.drawing.OverlayType.POLYGON;
-                //     }
-                // }
-
-                $scope.drawingManagerOptions = {
-                    drawingMode: initialDrawingMode,
-                    drawingControl: true,
-                    drawingControlOptions: {
-                        position: google.maps.ControlPosition.TOP_CENTER,
-                        drawingModes: availableDrawingModes
-                    },
-                    markerOptions: {
-                        draggable: true,
-                        animation: google.maps.Animation.DROP
-                    },
-                    events: {
-                        markercomplete: function (drawingManager, eventName, model, args) {
-                            clearCurrentMarkersAndLines();
-                            $scope.marker = args[0];
-
-                            var position = $scope.marker.position.lat() + ', ' + $scope.marker.position.lng();
-                            $scope.mapValue.updateLocation(position);
-                            reverseGeocode($scope.marker.position, function () {
-                                changeModel();
-                            });
-                        },
-                        polylinecomplete: function (drawingManager, eventName, model, args) {
-                            clearCurrentMarkersAndLines();
-                            $scope.polyline = args[0];
-
-                            $scope.mapValue.updateLocation($scope.polyline.getPath().getArray().toString());
-                            reverseGeocode($scope.polyline.getPath().getArray()[0], function () {
-                                changeModel();
-                            });
-                        },
-                        polygoncomplete: function (drawingManager, eventName, model, args) {
-                            clearCurrentMarkersAndLines();
-                            $scope.polygon = args[0];
-
-                            $scope.mapValue.updateLocation($scope.polygon.getPath().getArray().toString());
-                            reverseGeocode($scope.polygon.getPath().getArray()[0], function () {
-                                changeModel();
-                            });
-                        },
-                        drawingmode_changed: function () {
-                            //clearCurrentMarkersAndLines();
-                        }
-                    }
-                };
-
-            };
-            var createMapValue = function (value) {
-                if (value && !value.location) {
-                    value = JSON.parse(value);
-                } else {
-                    value = {location: '', street: '', zipcode: '', city: '', country: ''};
-                }
-                var mapValue = new MapValue(value);
-                mapValue.updateLocation(value.location);
-                return mapValue;
-            };
-
-            initMap();
-
-        },
-        link: function ($scope, NgMap) {
-            $scope.modelLoaded = false;
+        controller: function ($scope, NgMap) {
 
             var geocoder = null;
 
@@ -317,6 +137,16 @@ angular.module('sf').directive('googleMap', function () {
                 $scope.$apply();
             }
 
+            var createMapValue = function (value) {
+                if (value && !value.location) {
+                    value = JSON.parse(value);
+                } else {
+                    value = {location: '', street: '', zipcode: '', city: '', country: ''};
+                }
+                var mapValue = new MapValue(value);
+                mapValue.updateLocation(value.location);
+                return mapValue;
+            };
 
             var clearCurrentMarkersAndLines = function () {
                 if ($scope.marker) {
@@ -330,7 +160,119 @@ angular.module('sf').directive('googleMap', function () {
                 }
             };
 
+            var initMap = function () {
+                $scope.mapValue = createMapValue($scope.ngModel);
 
+                // TODO: Read startPosition and zoomLevel from settings
+                var startPosition;
+
+                if ($scope.mapValue.value.location) {
+                    startPosition = new LatLong($scope.mapValue.value.location);
+                } else {
+                    startPosition = new LatLong($scope.locationSettings.location);
+                }
+
+                var mapOptions = {
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    panControl: false,
+                    streetViewControl: false,
+                    overviewMapControl: false,
+                    scrollwheel: false
+                };
+
+                $scope.mapData = {
+                    center: startPosition,
+                    zoom: $scope.locationSettings.zoomLevel,
+                    options: mapOptions,
+                    control: {},
+                    markersControl: {}
+                };
+
+                var availableDrawingModes = [];
+                var initialDrawingMode = null;
+
+                if ($scope.mapValue) {
+                    if ($scope.mapValue.isPoint) {
+                        initialDrawingMode = google.maps.drawing.OverlayType.MARKER;
+                    } else if ($scope.mapValue.isPolyline) {
+                        initialDrawingMode = google.maps.drawing.OverlayType.POLYLINE;
+                    } else if ($scope.mapValue.isPolygon) {
+                        initialDrawingMode = google.maps.drawing.OverlayType.POLYGON;
+                    }
+                }
+
+                if ($scope.fieldSettings.point) {
+                    availableDrawingModes.push(google.maps.drawing.OverlayType.MARKER);
+                    if (!initialDrawingMode) {
+                        initialDrawingMode = google.maps.drawing.OverlayType.MARKER;
+                    }
+                }
+                if ($scope.fieldSettings.polyline) {
+                    availableDrawingModes.push(google.maps.drawing.OverlayType.POLYLINE);
+                    if (!initialDrawingMode) {
+                        initialDrawingMode = google.maps.drawing.OverlayType.POLYLINE;
+                    }
+                }
+                if ($scope.fieldSettings.polygon) {
+                    availableDrawingModes.push(google.maps.drawing.OverlayType.POLYGON);
+                    if (!initialDrawingMode) {
+                        initialDrawingMode = google.maps.drawing.OverlayType.POLYGON;
+                    }
+                }
+
+                console.log('FIeld settings',$scope.fieldSettings);
+                $scope.drawingManagerOptions = {
+                    drawingMode: initialDrawingMode,
+                    drawingControl: true,
+                    drawingControlOptions: {
+                        position: google.maps.ControlPosition.TOP_CENTER,
+                        drawingModes: availableDrawingModes
+                    },
+                    markerOptions: {
+                        draggable: true,
+                        animation: google.maps.Animation.DROP
+                    },
+                    events: {
+                        markercomplete: function (drawingManager, eventName, model, args) {
+                            clearCurrentMarkersAndLines();
+                            $scope.marker = args[0];
+
+                            if($scope.marker) {
+                                var position = $scope.marker.position.lat() + ', ' + $scope.marker.position.lng();
+                            } else {
+                                var position = "current-location";
+                            }
+                            $scope.mapValue.updateLocation(position);
+                            reverseGeocode($scope.marker.position, function () {
+                                changeModel();
+                            });
+                        },
+                        polylinecomplete: function (drawingManager, eventName, model, args) {
+                            clearCurrentMarkersAndLines();
+                            $scope.polyline = args[0];
+
+                            $scope.mapValue.updateLocation($scope.polyline.getPath().getArray().toString());
+                            reverseGeocode($scope.polyline.getPath().getArray()[0], function () {
+                                changeModel();
+                            });
+                        },
+                        polygoncomplete: function (drawingManager, eventName, model, args) {
+                            clearCurrentMarkersAndLines();
+                            $scope.polygon = args[0];
+
+                            $scope.mapValue.updateLocation($scope.polygon.getPath().getArray().toString());
+                            reverseGeocode($scope.polygon.getPath().getArray()[0], function () {
+                                changeModel();
+                            });
+                        },
+                        drawingmode_changed: function () {
+                            //clearCurrentMarkersAndLines();
+                        }
+                    }
+                };
+                console.log('drawingManagerOptions',$scope.drawingManagerOptions);
+
+            };
 
             var init = function (map) {
                 if ($scope.mapValue) {
@@ -472,16 +414,12 @@ angular.module('sf').directive('googleMap', function () {
                 }
             };
 
-            $scope.$watch('modelLoaded', function (newVal) {
-                if (newVal) {
-                    NgMap.getMap();/*.then(function (map) {
-                        map.setZoom($scope.map.zo$om);
-                        $scope.mapValue.updateLocation($scope.ngModel.location);
-                    });*/
-                }
+            initMap();
+
+            NgMap.getMap().then(function (map) {
+                map.setZoom($scope.locationSettings.zoomLevel);
+                map.mapDrawingManager[0].setOptions($scope.drawingManagerOptions);
             });
-
         }
-
     };
 });

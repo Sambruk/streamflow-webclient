02f3c531d64602932f0419002674373ca0ca67cd
3emluk <3emluk@gmail.com>
2018-01-31 14:17:50 +0200

    SF-96: Update license stuff: - Update license headings

--- build.bat
+++ build.bat
@@ -1,3 +1,22 @@
+@REM
+@REM
+@REM Copyright
+@REM 2009-2015 Jayway Products AB
+@REM 2016-2018 Föreningen Sambruk
+@REM
+@REM Licensed under AGPL, Version 3.0 (the "License");
+@REM you may not use this file except in compliance with the License.
+@REM You may obtain a copy of the License at
+@REM
+@REM     http://www.gnu.org/licenses/agpl.txt
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
+@REM
+
 ::
 ::
 :: Copyright 2009-2015 Jayway Products AB

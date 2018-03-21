c1395e29cfdbce7ccb08b99c9f4a2ab89945a948
3emluk <3emluk@gmail.com>
2018-03-21 17:47:49 +0200

    SF-83: Upgrade webclient dependencies: - Fix use strict statement

--- app/components/sidebar/sidebar-due-date-directive.js
+++ app/components/sidebar/sidebar-due-date-directive.js
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict'
+'use strict';
 angular.module('sf').directive('sidebarDueDate', function (sidebarService) {
     return {
         restrict: 'A',
